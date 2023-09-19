8996aff76e0e71603390c5e60f977b5c86fdbc48
Chamud Geesara Pathirana <71936025+Chamud@users.noreply.github.com>
2023-09-19 17:28:24 +0900

    Add serializer

--- Nokosu2023/api/views.py
+++ Nokosu2023/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import InfoSerializer
+from .models import Info
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -9,3 +11,9 @@ def getRoutes(request):
         }
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getInfos(request):
+    infos = Info.objects.all()
+    serializer = InfoSerializer(infos, many=True)
+    return Response(serializer.data)
