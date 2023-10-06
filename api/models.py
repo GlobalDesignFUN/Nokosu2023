332dfd76f52ecb5fa81d488b5a7fc8540261fe32
Chamud Geesara Pathirana <71936025+Chamud@users.noreply.github.com>
2023-10-06 15:11:50 +0900

    Modified models

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='static\img\profile_pics', blank=True, null=True)
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
@@ -17,9 +19,9 @@ class Info(models.Model):
     topic = models.TextField()
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to='static\img', blank=True, null=True)
+    photo = models.ImageField(upload_to='static\img\info', blank=True, null=True)
     location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
-    createdBy = models.ForeignKey()
+    createdBy = models.ForeignKey(Profile, on_delete=models.CASCADE,null=False)
 
     def __str__(self):
         return self.topic
