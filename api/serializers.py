1097419d0248991930ce17f1c46bb50dc9daa0aa
Chamud Geesara Pathirana <71936025+Chamud@users.noreply.github.com>
2023-10-06 16:49:54 +0900

    Add serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Info
+from .models import Info, Location, Profile
 
 class InfoSerializer(ModelSerializer):
     class Meta:
         model = Info
+        fields = '__all__' 
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__' 
+
+class LocationSerializer(ModelSerializer):
+    class Meta:
+        model = Location
         fields = '__all__' 
